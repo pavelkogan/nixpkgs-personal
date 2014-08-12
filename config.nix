5e5443ac569ac9f87e7a42bd2adf8bb6d20217ca
Pavel Kogan <pkogan@gmail.com>
2014-08-12 13:19:54 +0100

    Change dev env to use ghcWithPackages

--- config.nix
+++ config.nix
@@ -3,17 +3,20 @@
   packageOverrides = pkgs: rec {
     base = { inherit (pkgs) nix; };
     haskellPackages = with pkgs.haskellPackages; pkgs.haskellPackages // rec {
+      basePkgs = [ basePrelude shelly ];
+      nsEnv = ghcWithPackages (self: basePkgs ++ [
+        conduit dataDefault fgl graphviz gtk mtl parsec reactiveBanana
+        reactiveBananaGtk safe strict text transformers webkit xmlConduit
+        xmlConduitWriter xmlTypes
+      ]);
       baseDev = base // {
-        inherit ghc basePrelude cabalInstall cabal2nix shelly;
+        inherit cabalInstall cabal2nix;
       };
       toolDev = baseDev // {
-        inherit ghcMod hoogle hlint SourceGraph codex hasktags haddock;
+        inherit ghcMod hoogle hlint SourceGraph codex hasktags hscolour;
       };
       nsDev = toolDev // {
-        inherit
-          conduit dataDefault fgl graphviz gtk mtl parsec reactiveBanana
-          reactiveBananaGtk safe strict text transformers webkit xmlConduit
-          xmlConduitWriter xmlTypes;
+        inherit nsEnv;
         graphvizBin = pkgs.graphviz;
       };
       reactiveBananaGtk = callPackage ./haskell/reactive-banana-gtk.nix {};
