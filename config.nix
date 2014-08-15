2af8396d8884ef053e38abf653ca5c41686d4971
Pavel Kogan <pkogan@gmail.com>
2014-08-15 16:20:25 +0100

    Add data-default-generics

--- config.nix
+++ config.nix
@@ -5,7 +5,7 @@
     haskellPackages = with pkgs.haskellPackages; pkgs.haskellPackages // rec {
       basePkgs = [ basePrelude shelly basicPrelude ];
       nsEnv = ghcWithPackages (self: basePkgs ++ [
-        conduit dataDefault fgl graphviz gtk mtl parsec reactiveBanana
+        conduit dataDefaultGenerics fgl graphviz gtk mtl parsec reactiveBanana
         reactiveBananaGtk safe strict text transformers webkit xmlConduit
         xmlConduitWriter xmlTypes
       ]);
@@ -19,6 +19,7 @@
         inherit nsEnv;
         graphvizBin = pkgs.graphviz;
       };
+      dataDefaultGenerics = callPackage ./haskell/data-default-generics.nix {};
       reactiveBananaGtk = callPackage ./haskell/reactive-banana-gtk.nix {};
     };
   };
